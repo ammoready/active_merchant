9c4d28b5aac0196f13faf71278e8995bf199e94f
Jeffrey Dill <jeffdill2@gmail.com>
2018-12-12 18:06:09 -0500

    Protect transaction response from null data responses ammoready/ammoready-two-point-oh#335

--- lib/active_merchant/billing/gateways/blue_dog_v2.rb
+++ lib/active_merchant/billing/gateways/blue_dog_v2.rb
@@ -182,23 +182,23 @@ module ActiveMerchant #:nodoc:
       end
 
       def message_from(response)
-        response[:data][:response]
+        response[:data][:response] unless response[:data].nil?
       end
 
       def authorization_from(response)
-        response[:data][:id]
+        response[:data][:id] unless response[:data].nil?
       end
 
       def avs_result_from(response)
-        AVSResult.new(code: response[:data][:response_body][:card][:avs_response_code])
+        AVSResult.new(code: response[:data][:response_body][:card][:avs_response_code]) unless response[:data].nil?
       end
 
       def cvv_result_from(response)
-        CVVResult.new(response[:data][:response_body][:card][:cvv_response_code])
+        CVVResult.new(response[:data][:response_body][:card][:cvv_response_code]) unless response[:data].nil?
       end
 
       def error_code_from(response)
-        response[:data][:response_code] unless success_from(response)
+        response[:data][:response_code] unless success_from(response) || response[:data].nil?
       end
 
       def post_data(action, parameters)
